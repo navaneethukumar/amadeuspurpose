02caab6bbcb234bf7d12dd4fee8ae17df6b2fa12
Naresh kumar <nareskumar175@gmail.com>
2020-03-01 12:11:30 +0530

    added intervuiew questions

---
+++ src/test/java/test/StartChrome2.java
@@ -0,0 +1,57 @@
+package test;
+import java.net.MalformedURLException;
+import java.net.URL;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class StartChrome2
+{
+ public static void main(String[] args) throws MalformedURLException{
+ 
+// Create object of  DesiredCapabilities class and specify android platform
+DesiredCapabilities capabilities=DesiredCapabilities.android();
+ 
+ 
+// set the capability to execute test in chrome browser
+ capabilities.setCapability(MobileCapabilityType.BROWSER_NAME,BrowserType.CHROME);
+ 
+// set the capability to execute our test in Android Platform
+   capabilities.setCapability(MobileCapabilityType.PLATFORM,Platform.ANDROID);
+ 
+// we need to define platform name
+  capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME,"Android");
+ 
+// Set the device name as well (you can give any name)
+ capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"my phone");
+ 
+ // set the android version as well 
+   capabilities.setCapability(MobileCapabilityType.VERSION,"6.0.0");
+ 
+ // Create object of URL class and specify the appium server address
+ URL url= new URL("http://127.0.0.1:4723/wd/hub");
+ 
+// Create object of  AndroidDriver class and pass the url and capability that we created
+ WebDriver driver = new AndroidDriver(url, capabilities);
+ 
+// Open url
+  //driver.get("http://www.facebook.com");
+ 
+ // print the title
+//  System.out.println("Title "+driver.getTitle());
+ 
+
+ 
+// close the browser
+driver.quit();
+ 
+ 
+ 
+ 
+}
+ 
+}
---
+++ src/test/java/test/test1.java
@@ -0,0 +1,25 @@
+package test;
+
+public class test1 {
+	
+	
+	public static void main(String[] args) {
+		int a[]={1,1,0,0,1,0,1};
+		int j=0;
+		for(int i=0;i<a.length;i++){
+			if(a[i]==0){
+				a[j++]=a[i];}
+			
+		}
+		while(j<a.length){
+			a[j++]=1;
+			
+		}
+		for(int k=0;k<a.length;k++){
+			System.out.print(a[k]+ " ");
+			
+		}
+	}
+		
+	}
+
